085796f4eac1057dd9166f7244cecfbdacf3f00d
Andrew Montgomery-Hurrell <andrew.montgomery-hurrell@multiplay.co.uk>
2016-07-05 11:41:59 +0100

    Initial pass at rust-cypher gen

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-numenera-oddity-generator
-=========================
+numenera-rust-cypher-generator
+==============================
 
-An oddity generator for Numenera
+A rust-cypher generator for Numenera, based on the system introduced by the
+Iron Wind sourcebook "[Rusthaven][1]"
+
+[1]: http://www.drivethrurpg.com/product/185316/Rusthaven-An-Iron-Wind-Sourcebook-for-Numenera?affiliate_id=201046&src=rust-cypher-gen
--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 (function(document, Tabletop, location) {
   "use strict";
 
+  function uniques(arr) {
+    var a = [];
+    for (var i=0, l=arr.length; i<l; i++) {
+      if (a.indexOf(arr[i]) === -1 && arr[i] !== '') {
+        a.push(arr[i]);
+      }
+    }
+    return a;
+  }
+
+  function arrayToSentence(arr) {
+    if (arr.length === 1) {
+      return arr[0];
+    }
+    return arr.slice(0, arr.length - 1).join(', ') + ", and " + arr.slice(-1);
+  }
+
+  function randomNum(limit) {
+    return Math.floor(Math.random() * limit);
+  }
+
   function randomArrayElement(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
+    return arr[randomNum(arr.length)];
   }
 
   function capitaliseFirstLetter(string) {
@@ -11,34 +32,57 @@
 
   function tablestuff(data) {
     var button = document.getElementById("generate"),
-        text = document.getElementById("phrase");
+        descEl = document.getElementById("description"),
+        abilityEl = document.getElementById("ability"),
+        opsEl = document.getElementById("operation");
 
     function generate() {
-      var phrase = randomArrayElement(data.Phrase.elements).Phrase,
-          replacer = function(match, key) {
-            if(key) {
-              var column = key, row;
-              if(key.indexOf("-") > -1) {
-                var tmp = key.split("-");
-                key = tmp[0];
-                row = randomArrayElement(data[key].elements);
-                var result = row[tmp[1]];
-                if(result && result.length > 0) {
-                  return result;
-                } else {
-                  column = key;
-                }
-              } else {
-                row = randomArrayElement(data[key].elements);
-              }
-              return row[column];
-            } else {
-              return "ERROR";
-            }
-          };
+      var materials = [], shapes = [], looks = [], num;
 
-      var p = phrase.replace(/\%([A-Za-z\-]+)\%/g, replacer),
-          parts = p.split(" "),
+      // Materials
+      while (true) {
+        materials.push(randomArrayElement(data.Appearance.elements).Material);
+        num = randomNum(20);
+        if (num != 20) {
+          break;
+        }
+      }
+      materials = uniques(materials);
+
+      // Shapes
+      while (true) {
+        shapes.push(randomArrayElement(data.Appearance.elements).Shape);
+        num = randomNum(20);
+        if (num != 20) {
+          break;
+        }
+      }
+      shapes = uniques(shapes);
+
+      // Look & Feels
+      while (true) {
+        looks.push(randomArrayElement(data.Appearance.elements)["Look & Feel"]);
+        num = randomNum(20);
+        if (num != 20) {
+          break;
+        }
+      }
+      looks = uniques(looks);
+
+      var type = randomArrayElement(['Self', 'Area', 'Other']),
+          level = randomNum(6)+2,
+          description = [
+            'A',
+            arrayToSentence(looks),
+            'object made of',
+            arrayToSentence(materials),
+            'shaped like',
+            arrayToSentence(shapes)
+          ].join(' '),
+          ability = randomArrayElement(data.Abilities.elements)[type],
+          operation = randomArrayElement(data.Operation.elements)[type];
+
+      var parts = description.split(" "),
           result = [],
           isA = false;
       for(var i = 0; i < parts.length; i++) {
@@ -52,7 +96,9 @@
         }
         isA = (clean.toLowerCase() === "a");
       }
-      text.innerHTML = capitaliseFirstLetter(result.join(" "));
+      descEl.innerHTML = capitaliseFirstLetter(result.join(" "));
+      abilityEl.innerHTML = ability;
+      opsEl.innerHTML = operation;
     }
 
     button.onclick = generate;
@@ -68,7 +114,7 @@
   ].join('');
 
   Tabletop.init({
-    key: "19Zq00MK8IzIQfxzwl3cVXqevgv9RkhEFn9ypYpRwRkQ",
+    key: "1ubw8FovP_gHQdMX5XGJFAW1krRLClddZFy7K7BYt1F0",
     callback: tablestuff,
     simpleSheet: false
   });
